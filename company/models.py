f7331c3c3da0786416b16ed33169ccf0d4ba17b8
mohamedtarek87897toptdt <alpazmohamedtarek@gmail.com>
2024-11-27 19:18:06 +0200

    first commit

--- company/models.py
+++ company/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class branches(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100,help_text='enter full address')
+    phone = models.CharField(max_length=12,null=True)
+    email = models.EmailField(max_length=50,null=True)
+
+
+
+class departments(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200,help_text='wrait description adoutthe department')
+    branch = models.ForeignKey(branches,related_name='branch',on_delete=models.CASCADE)
+
+     
