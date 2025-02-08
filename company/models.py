84184ca1585d1c545d8bc3ed2e4bb17fae304d36
mohamedtarek87897toptdt <alpazmohamedtarek@gmail.com>
2025-02-08 16:36:04 +0200

    first commit

--- company/models.py
+++ company/models.py
@@ -7,11 +7,16 @@ class branches(models.Model):
     phone = models.CharField(max_length=12,null=True)
     email = models.EmailField(max_length=50,null=True)
 
+    def __str__(self):
+       return self.name
 
 
 class departments(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200,help_text='wrait description adoutthe department')
     branch = models.ForeignKey(branches,related_name='departmentsBranche',on_delete=models.CASCADE)
+
+    def __str__(self):
+       return self.name
 
      
